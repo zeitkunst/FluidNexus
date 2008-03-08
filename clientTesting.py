44adcd42be2abde55f8eb4430522e17a12c2ddd1
fluidnexus <fluidnexus@d200c93d-81fa-4693-bfd9-a8dd726a5e1d>
2008-03-08 12:16:09 +0000

    -- wrote a client that takes (right now) the first entry of those marked outgoing and sends it to other phones that are running fluid nexus -- need to update it to loop through each item marked as outgoing, and then mark those as ones that have been transferred
    git-svn-id: https://svn.zeitkunst.org/repos/FluidNexus/trunk@8 d200c93d-81fa-4693-bfd9-a8dd726a5e1d

--- clientTesting.py
+++ clientTesting.py
@@ -9,13 +9,76 @@ import time
 # other imports
 import lightblue
 
+# @HACK@
+# Adding paths to find the modules
+sys.path.append('.')
+sys.path.append(os.getcwd())
+sys.path.append('C:\\System\\Apps\\Python\\my\\')
+sys.path.append('E:\\System\\Apps\\Python\\my\\')
+sys.path.append('C:\\Python')
+sys.path.append('E:\\Python')
+
+from logger import Logger
+import database
+from database import FluidNexusDatabase
+
+# Series 60 specific imports
+try:
+    # On phone?
+    import e32
+    import e32db
+
+    # @SEMI-HACK@
+    # At the moment, set global variable that determines where our data is      going to live
+    try:
+        os.listdir('E:')
+        dataPath = u'E:\\System\\Data\\FluidNexusData'
+    except OSError:
+        dataPath = u'C:\\System\\Data\\FluidNexusData'
+
+    # Setup our data path
+    if not os.path.isdir(dataPath):
+        os.makedirs(dataPath)
+
+    # Setup logging and redirect standard input and output
+    log = Logger(dataPath + u'\\FluidNexus.log', prefix = 'FluidNexus Networking: ')
+    #sys.stderr = sys.stdout = log
+
+    onPhone = True
+except ImportError:
+    #from s60Compat import e32
+    dataPath = '.'
+    log = Logger(dataPath + u'\\FluidNexus.log', prefix = 'FluidNexusServer: ')
+    #sys.stderr = sys.stdout = log
+
+    onPhone = False
+
+
 class FluidNexusClient(object):
-    def __init__(self):
-        # Nothing here right now...
-        pass
-    
-    def run(self):
-        allDevices = lightblue.finddevices(length = 5)
+    def __init__(self, database = None):
+        self.db = database
+   
+    def splitclass(self, deviceClass):
+        """Taken from lightblue library."""
+
+        if not isinstance(deviceClass, int):
+            try:
+                deviceClass = int(deviceClass)
+            except (TypeError, ValueError):
+                raise TypeError("Given device class '%s' cannot be split" % \
+                                str(deviceClass))
+
+        data = deviceClass >> 2   # skip over the 2 "format" bits
+        service = data >> 11
+        major = (data >> 6) & 0x1F
+        minor = data & 0x3F
+        return (service, major, minor)
+
+
+    def runLightblue(self):
+        print "looking for devices"
+        #allDevices = socket.bt_discover()
+        allDevices = lightblue.finddevices()
         phones = []
 
         for device in allDevices:
@@ -28,6 +91,60 @@ class FluidNexusClient(object):
             services = lightblue.findservices(phone[0])
             print services
             for service in services:
+                if service[2] is not None and service[2] == 'FluidNexus':
+                    port = service[1]
+                    break
+                else:
+                    port = None
+            print port
+            print "at end of service search"
+
+            if port is not None:
+                print "sending data!"
+                self.db.outgoing()
+                listItems = []
+
+                for item in database:
+                    listItems.append(item)
+
+                print listItems
+
+                messageTime = listItems[0][2]
+                title = listItems[0][4]
+                message = listItems[0][5]
+
+                clientSocket = socket.socket(socket.AF_BT, socket.SOCK_STREAM)
+                clientSocket.connect((phone[0], port))
+                clientSocket.send('01')
+                time.sleep(1)
+                clientSocket.send("%03d" % len(title))
+                time.sleep(1)
+                clientSocket.send("%06d" % len(message))
+                time.sleep(1)
+                clientSocket.send(str(messageTime))
+                time.sleep(1)
+                clientSocket.send(md5.md5(title + message).hexdigest())
+                time.sleep(1)
+                clientSocket.send(title)
+                time.sleep(1)
+                clientSocket.send(message)
+                time.sleep(1)
+                clientSocket.close()
+
+    def run(self):
+        allDevices = socket.bt_discover()
+        phones = []
+
+        for device in allDevices:
+            foo, isPhone, bar = self.splitclass(device[2])
+            if isPhone == 2:
+                phones.append(device)
+
+        for phone in phones:
+            print "Looking at phone", phone
+            services = socket.bt_discover(phone[0])
+            print services
+            for service in services:
                 if service[2] is not None and 'FluidNexus' in service[2]:
                     port = service[1]
                     break
@@ -38,7 +155,7 @@ class FluidNexusClient(object):
 
             if port is not None:
                 print "sending data!"
-                clientSocket = lightblue.socket()
+                clientSocket = socket.socket(socket.AF_BT, socket.SOCK_STREAM)
                 clientSocket.connect((phone[0], port))
                 clientSocket.send('01')
                 time.sleep(1)
@@ -57,5 +174,6 @@ class FluidNexusClient(object):
                 clientSocket.close()
 
 if __name__ == "__main__":
-    client = FluidNexusClient()
-    client.run()
+    database = FluidNexusDatabase()
+    client = FluidNexusClient(database = database)
+    client.runLightblue()
