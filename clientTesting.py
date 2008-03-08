16f6af04dc1e6b36f4921f9029eef0a427f883a1
fluidnexus <fluidnexus@d200c93d-81fa-4693-bfd9-a8dd726a5e1d>
2008-03-08 19:54:45 +0000

    -- making some new code for sending, as we check the database first
    git-svn-id: https://svn.zeitkunst.org/repos/FluidNexus/trunk@13 d200c93d-81fa-4693-bfd9-a8dd726a5e1d

--- clientTesting.py
+++ clientTesting.py
@@ -22,6 +22,8 @@ from logger import Logger
 import database
 from database import FluidNexusDatabase
 
+FLUID_NEXUS_VERSION = '01'
+
 # Series 60 specific imports
 try:
     # On phone?
@@ -74,6 +76,64 @@ class FluidNexusClient(object):
         minor = data & 0x3F
         return (service, major, minor)
 
+    def sendData(self, data, phone, port):
+        """Send our data to the other phone!"""
+
+        messageTime = data[2]
+        title = data[4]
+        message = data[5]
+
+        clientSocket = socket.socket(socket.AF_BT, socket.SOCK_STREAM)
+        clientSocket.connect((phone[0], port))
+        clientSocket.send(FLUID_NEXUS_VERSION)
+        time.sleep(1)
+        clientSocket.send("%03d" % len(title))
+        time.sleep(1)
+        clientSocket.send("%06d" % len(message))
+        time.sleep(1)
+        clientSocket.send(str(messageTime))
+        time.sleep(1)
+        clientSocket.send(md5.md5(title + message).hexdigest())
+        time.sleep(1)
+        clientSocket.send(title)
+        time.sleep(1)
+        clientSocket.send(message)
+        time.sleep(1)
+        clientSocket.close()
+
+
+    def getOurMessageHashes(self):
+        """Return a list of the messages marked as 'mine'."""
+        ourMessageHashes = []
+
+        self.db.outgoing()
+
+        for item in self.db:
+            ourMessageHashes.append(item[6])
+
+        return ourMessageHashes
+
+    def getNotOurMessageHashes(self):
+        """Return a list of the messages marked as 'mine'."""
+        notOurMessageHashes = []
+
+        self.db.non_outgoing()
+
+        for item in self.db:
+            notOurMessageHashes.append(item[6])
+
+        return notOurMessageHashes
+
+
+    def getServerMessageHashes(self, services):
+        """Loop through the services that we've gotten and return a list of hashes that we've seen."""
+        serverMessageHashes = []
+        
+        for service in services:
+            if service[2][0] == ':':
+                serverMessageHashes.append(service[2][1:])
+
+        return serverMessageHashes
 
     def runLightblue(self):
         print "looking for devices"
@@ -89,47 +149,43 @@ class FluidNexusClient(object):
         for phone in phones:
             print "Looking at phone", phone
             services = lightblue.findservices(phone[0])
-            print services
             for service in services:
                 if service[2] is not None and service[2] == 'FluidNexus':
                     port = service[1]
                     break
                 else:
                     port = None
-            print port
             print "at end of service search"
 
             if port is not None:
-                print "sending data!"
-                self.db.outgoing()
-                listItems = []
+                serverMessageHashes = self.getServerMessageHashes(services)
+                ourMessageHashes = self.getOurMessageHashes()
+                notOurMessageHashes = self.getNotOurMessageHashes()
+                hashesToSend = []
 
-                for item in database:
-                    listItems.append(item)
+                # First, check if any of our outgoing messages are already on
+                # the server
+                for hash in serverMessageHashes:
+                    if hash in ourMessageHashes:
+                        ourMessageHashes.remove(hash)
+                hashesToSend.extend(ourMessageHashes)
 
-                print listItems
+                # Then, check if any of our other messages are on the server
+                for hash in serverMessageHashes:
+                    if hash in notOurMessageHashes:
+                        notOurMessageHashes.remove(hash)
+                hashesToSend.extend(notOurMessageHashes)
 
-                messageTime = listItems[0][2]
-                title = listItems[0][4]
-                message = listItems[0][5]
+                # Thus we have a precedence where the ones that we have created will appear first in the list
 
-                clientSocket = socket.socket(socket.AF_BT, socket.SOCK_STREAM)
-                clientSocket.connect((phone[0], port))
-                clientSocket.send('01')
-                time.sleep(1)
-                clientSocket.send("%03d" % len(title))
-                time.sleep(1)
-                clientSocket.send("%06d" % len(message))
-                time.sleep(1)
-                clientSocket.send(str(messageTime))
-                time.sleep(1)
-                clientSocket.send(md5.md5(title + message).hexdigest())
-                time.sleep(1)
-                clientSocket.send(title)
-                time.sleep(1)
-                clientSocket.send(message)
-                time.sleep(1)
-                clientSocket.close()
+                if hashesToSend != []:
+                    print "sending data!"
+                    for hash in hashesToSend:
+                        data = self.db.returnItemBasedOnHash(hash)
+                        print data
+                        self.sendData(data, phone, port)
+                else:
+                    print "no data to send"
 
     def run(self):
         allDevices = socket.bt_discover()
