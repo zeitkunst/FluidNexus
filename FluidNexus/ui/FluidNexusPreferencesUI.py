bd121ef400b270022623d31be340d786779a800d
zeitkunst <nknouf@zeitkunst.org>
2011-07-24 12:34:10 -0400

    -- correct location for public_other icon

--- FluidNexus/ui/FluidNexusPreferencesUI.py
+++ FluidNexus/ui/FluidNexusPreferencesUI.py
@@ -2,7 +2,7 @@
 
 # Form implementation generated from reading ui file 'FluidNexus/ui/FluidNexusPreferences.ui'
 #
-# Created: Sun Jul 24 12:31:05 2011
+# Created: Sun Jul 24 12:34:07 2011
 #      by: PyQt4 UI code generator 4.8.1
 #
 # WARNING! All changes made in this file will be lost!
