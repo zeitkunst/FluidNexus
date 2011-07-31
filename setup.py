8b08260efa3c2497f4a13cec6feeef66fd5f347c
zeitkunst <nknouf@zeitkunst.org>
2011-07-30 20:12:38 -0400

    -- updating changelog

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ import FluidNexus
 
 here = os.path.abspath(os.path.dirname(__file__))
 README = open(os.path.join(here, 'README.rst')).read()
-CHANGES = open(os.path.join(here, 'CHANGES')).read()
+CHANGES = open(os.path.join(here, 'CHANGES.rst')).read()
 
 requires = [
         'textile',
